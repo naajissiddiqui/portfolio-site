4fd06ebdfd51d229451149cec960d7c495f3ea91
Naajis Siddiqui <naajissiddiqui123@gmail.com>
2025-10-02 22:54:40 +0530

    updated navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FiMenu, FiX } from "react-icons/fi";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [activeSection, setActiveSection] = useState("");
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  //Check scroll and change navbar bg
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  //Smooth scroll function
+  const handleMenuItemClick = (sectionId) => {
+    setActiveSection(sectionId);
+    setIsOpen(false);
+  };
+
   const menuItems = [
     { id: "about", label: "About" },
     { id: "skills", label: "Skills" },
@@ -10,7 +29,13 @@ const Navbar = () => {
     { id: "education", label: "Education" },
   ];
   return (
-    <nav className="bg-transparent">
+    <nav
+      className={`fixed top-0 w-full z-50 transition duration-300 px-[7vw] lg:px-[20vw] ${
+        isScrolled
+          ? "bg-[#050414] bg-opacity-50 backdrop-blur-md shadow-md"
+          : "bg-transparent"
+      } `}
+    >
       <div className="text-white py-5 flex justify-between items-center">
         {/* Logo */}
         <div className="text-lg font-semibold cursor-pointer">
@@ -24,11 +49,28 @@ const Navbar = () => {
         {/* Desktop Menu */}
         <ul className="md:flex space-x-8 text-gray-300">
           {menuItems.map((item) => (
-            <li key={item.id} className="">
-              <button>{item.label}</button>
+            <li
+              key={item.id}
+              className={`cursor-pointer hover:text-[#8245ec] ${
+                activeSection == item.id ? "text-[#8245ec]" : ""
+              }`}
+            >
+              <button onClick={() => handleMenuItemClick(item.id)}>
+                {item.label}
+              </button>
             </li>
           ))}
         </ul>
+
+        {/* Scroll Media icons */}
+        <div className="hidden md:flex space-x-4">
+          <a
+            href="https://github.com/naajisssiddiqui"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-[#8245ec]"
+          ></a>
+        </div>
       </div>
     </nav>
   );
