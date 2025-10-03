c994732b7e61ac2cf8462eae4319c3c7ff6f7276
Naajis Siddiqui <naajissiddiqui123@gmail.com>
2025-10-03 22:35:40 +0530

    updated navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -65,13 +65,35 @@ const Navbar = () => {
         {/* Scroll Media icons */}
         <div className="hidden md:flex space-x-4">
           <a
-            href="https://github.com/naajisssiddiqui"
+            href="https://github.com/naajissiddiqui"
             target="_blank"
             rel="noopener noreferrer"
             className="text-gray-300 hover:text-[#8245ec]"
           >
             <FaGithub size={24} />
           </a>
+          <a
+            href="https://github.com/naajissiddiqui"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-[#8245ec]"
+          >
+            <FaLinkedin size={24} />
+          </a>
+        </div>
+        {/* Mobile Menu Icons */}
+        <div className="md:hidden">
+          {isOpen ? (
+            <FiX
+              className="text-3xl text-[#8245ec] cursor-pointer"
+              onClick={() => setIsOpen(false)}
+            />
+          ) : (
+            <FiMenu
+              className="text-3xl text-[#8245ec] cursor-pointer"
+              onClick={() => setIsOpen(true)}
+            />
+          )}
         </div>
       </div>
     </nav>
