1be9a99b77919f3285d5a356daed6e8ca56d3a8e
Naajis Siddiqui <naajissiddiqui123@gmail.com>
2025-10-08 22:44:28 +0530

    added minor stuff

---
+++ src/BlurBlob.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const BlurBlob = () => {
+  return (
+    <div
+      className="absolute "
+      style={{
+        top: top,
+        left: left,
+        width: width,
+        height: height,
+        transform: "translate(-50%, -50%)",
+      }}
+    ></div>
+  );
+};
+
+export default BlurBlob;
