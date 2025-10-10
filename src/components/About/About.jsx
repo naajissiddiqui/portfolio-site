1d9169b154f4cf63992a84a5e6de4f9365f30900
Naajis Siddiqui <naajissiddiqui123@gmail.com>
2025-10-10 23:41:40 +0530

    updated about.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return;
+  <section
+    id="about"
+    className="py-4 px-[7vw] md:px-[7vw] lg:px-[20vw"
+    font-sans
+    mt-16
+    md:mt-24
+    lg:mt-32
+  ></section>;
 };
 
 export default About;
