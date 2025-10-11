20178b4a99debf4813e9d2799e90b3a3f21d4aab
Naajis Siddiqui <naajissiddiqui123@gmail.com>
2025-10-11 23:18:13 +0530

    modeified app.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,12 +7,11 @@ const About = () => {
       className="py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32"
     >
       <div className="flex flex-col-reverse md:flex-row justify-between items-center">
-        <div className="w-full text-center mt-8 md:mt-0">
+        <div className="md:w-1 text-center md:text-center mt-8 md:mt-0">
           {/* Greeting */}
           <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold text-white mb-2 leading-tight">
             Hi, I am
           </h1>
-
           {/* Name */}
           <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold text-white mb-4 leading-tight">
             Naajis Siddiqui
