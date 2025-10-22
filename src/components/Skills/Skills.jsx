096dfa0f36d3941a05229eb896a3d0785459de99
Naajis Siddiqui <naajissiddiqui123@gmail.com>
2025-10-22 22:30:54 +0530

    update skills.jsx

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { SkillsInfo } from "../../constants";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return;
+  <section
+    id="skills"
+    className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[20vw] font-sans "
+  ></section>;
 };
 
 export default Skills;
