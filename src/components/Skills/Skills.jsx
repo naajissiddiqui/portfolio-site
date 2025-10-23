00ec3067a28d8277b56c0d16f437b5d005c416ba
Naajis Siddiqui <naajissiddiqui123@gmail.com>
2025-10-23 22:37:18 +0530

    update skills.jsx

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -5,8 +5,19 @@ const Skills = () => {
   return;
   <section
     id="skills"
-    className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[20vw] font-sans "
-  ></section>;
+    className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[20vw] font-sans bg-skills-gradient"
+  >
+    {/* Section title */}
+    <div className="text-center mb-8">
+      <h2 className="text-3xl sm:text-4xl font-bold text-white">Skills</h2>
+      <div className="w-24 h1 bg-[#8245ec] mx-auto mt-2"></div>
+
+      <p>
+        A collection of my technical skills and expertise honed through various
+        projects and experiences
+      </p>
+    </div>
+  </section>;
 };
 
 export default Skills;
