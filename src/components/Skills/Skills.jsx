6fc17888c1dc0a0e581d8e64a718f2981ea32800
Naajis Siddiqui <naajissiddiqui123@gmail.com>
2025-10-24 21:18:40 +0530

    .

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -12,10 +12,20 @@ const Skills = () => {
       <h2 className="text-3xl sm:text-4xl font-bold text-white">Skills</h2>
       <div className="w-24 h1 bg-[#8245ec] mx-auto mt-2"></div>
 
-      <p>
+      <p className="text-gray-400 mt-4 text-lg font-semibold ">
         A collection of my technical skills and expertise honed through various
         projects and experiences
       </p>
+    </div>
+
+    {/* Skills Categories */}
+    <div className="flex flex-wrap gap-1 lg:gap-5 py-10 justify-between ">
+      {SkillsInfo.map((category) => (
+        <div
+          key={category.title}
+          className="bg-gray-900 backdrop-blur-md px-6 sm:px-10  py-8 sm:py-6 mb-10 w-full sm:w-[48%] rounded-2xl border border-white shadow-[0_0_20px_1px_rgba(130, 69, 236, 0.3)] "
+        ></div>
+      ))}
     </div>
   </section>;
 };
