342bc4f58cd19415ea57fcebf92c4be105273427
Naajis Siddiqui <naajissiddiqui123@gmail.com>
2025-11-04 23:02:16 +0530

    Started Experience.jsx

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { experiences } from "../../constants";
 
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <section
+      id="experience"
+      className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[16vw] font-sans bg-skills-gradient "
+    >
+      {/* Section Title */}
+      <div className="text-center mb-16">
+        <h2 className="text-4xl font-bold text-white ">EXPERIENCE</h2>
+        <div className="w-32 h-1 bg-purple-500 mx-auto mt-4"></div>
+        <p className="text-gray-400 mt-4 text-lg font-semibold">
+          A collection of my work
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
